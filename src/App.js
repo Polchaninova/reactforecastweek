b04526a97397e72041320f9615ac367ac17af8ba
Polchaninova <107672221+Polchaninova@users.noreply.github.com>
2022-11-15 12:47:09 +0200

    instal bootstrap and add conect link

--- src/App.js
+++ src/App.js
@@ -4,8 +4,11 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Search Engune</h1>
-      <footer>GitHub</footer>
+      <div className="container">
+        <h1>Search Engune</h1>
+        <button className="btn btn-primary">Cat</button>
+        <footer>GitHub</footer>
+      </div>
     </div>
   );
 }
