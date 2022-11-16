98bcc784a319c96eb6fcc68ff6128dd66a18da5e
Polchaninova <107672221+Polchaninova@users.noreply.github.com>
2022-11-16 21:20:32 +0200

    adder content to my project

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from "react";
 import "./App.css";
-import Weather from "./Weather"
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Search Engune</h1>
-        <Weather/>
-        <button className="btn btn-primary">Cat</button>
-        <footer>GitHub</footer>
+        {/* <h1>Search Engune</h1> */}
+        <Weather />
+        {/* <button className="btn btn-primary">Cat</button> */}
+        {/* <footer>GitHub</footer> */}
       </div>
     </div>
   );
--- src/Weather.js
+++ src/Weather.js
@@ -1,14 +1,49 @@
 import React from "react";
-import "./App.css";
+import "./weather.css";
 
 export default function Weather() {
   return (
-    <div className="App">
-      <div className="container">
-        <h1>Search Engune</h1>
-        <button className="btn btn-primary">Cat</button>
-        <footer>GitHub</footer>
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city.."
+              className="form-control"
+            />
+          </div>
+          <div className="col-3">
+            <input type="submit" value="Search" className="btn btn-primary" />
+          </div>
+        </div>
+      </form>
+      <h1>New York</h1>
+      <ul>
+        <li>Wednesday 07:00</li>
+        <li>Nostly Cloudy</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/rain_light.png"
+            alt="Nostly Cloudy"
+          />
+          4°C
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Preciptation: 15%</li>
+            <li>Humidity 59%</li>
+            <li>Wind WNW 14 mph</li>
+          </ul>
+        </div>
       </div>
+
+      <footer>
+        This project was coded by Math Delac and is open-sourced on GitHub
+        GitHub
+      </footer>
     </div>
   );
 }
