959d621807db9c99e01ac09d17cf5586eacdd5b1
Olha Polchaninova <polchaninovaolha089@gmail.com>
2022-11-24 14:22:37 +0200

    added new component and create a Forecast  Days

--- src/Weather.ForecastDay.js
+++ src/Weather.ForecastDay.js
@@ -10,12 +10,16 @@ export default function WeatherForecastDay(props) {
     let temperature = Math.round(props.data.temp.mix);
     return `${temperature}°`;
   }
-  function day{
+  function day() {
+    let date = new Date(props.data.dt * 1000);
+    let day = date.getDay();
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
     
+    return days[day];
   }
   return (
     <div>
-      <div className="WeatherForecast-day">{props.data.dt}</div>
+      <div className="WeatherForecast-day">{day()}</div>
       <WeatherIcons code={props.data.weather[0].icon} size={36} />
       <div className="WeatherForecast-temperatures">
         <span className="WeatherForecast-temperature-max">
