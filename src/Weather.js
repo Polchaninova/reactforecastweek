8b7dac4566ba636b94bdf6e76a909ae689c2be1d
Olha Polchaninova <polchaninovaolha089@gmail.com>
2022-11-24 11:19:08 +0200

    added css to progect , created new component WeatherForecast and added content

--- src/Weather.js
+++ src/Weather.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from "react";
 import WeatherInfo from "./WeatherInfo";
 import "./weather.css";
 import axios from "axios";
+import WeatherForecast from "./WeatherForecast";
 
 export default function Weather(props) {
   const [city, setCity] = useState(props.defaultCity);
@@ -66,6 +67,7 @@ export default function Weather(props) {
           </div>
         </form>
         <WeatherInfo data={weatherData} />
+        <WeatherForecast />
       </div>
     );
   } else {
